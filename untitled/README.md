fae88ff060e11dd262f01e176c44ccd61970ea67
shkid-sketch <shouryazade639@gmail.com>
2025-09-29 09:00:37 -0700

    Delete untitled directory

--- untitled/README.md
+++
@@ -1,6 +0,0 @@
-# Untitled
-
-A Pen created on CodePen.
-
-Original URL: [https://codepen.io/Shourya-Ambagade/pen/bNEpjbM](https://codepen.io/Shourya-Ambagade/pen/bNEpjbM).
-
--- untitled/dist/script.js
+++
@@ -1,7 +0,0 @@
-function toggleAbout() {
-  const aboutSection = document.getElementById("about");
-  const aboutBtn = document.getElementById("aboutBtn");
-
-  aboutSection.classList.remove("hidden"); // Show the about section
-  aboutBtn.style.display = "none";         // Hide the About Me button
-}
